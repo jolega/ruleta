93810f02beff4b413f9c064c6f6cb9e7a6774824
johan <alonso_juancho@hotmail.com>
2020-07-05 11:58:42 -0500

    creacion del proyecto

---
+++ src/ruleta/Bet.java
@@ -0,0 +1,11 @@
+package ruleta;
+public class Bet {
+	int number;
+	String color;
+	int value;	
+	public Bet(int number,String color, int value){
+		number=this.number;
+		color=this.color;
+		value=this.value;		
+	}
+}
---
+++ src/ruleta/Roulette.java
@@ -0,0 +1,11 @@
+package ruleta;
+
+import java.util.ArrayList;
+
+public class Roulette {
+	 int id;	
+	 boolean estado;
+	 ArrayList<Roulette> bets = new ArrayList<Roulette>();
+	 public Roulette(){		 
+	 }	
+}
